cd08d66f38055a2b4561cb169665f06779ca77d0
Saiman Saxena <saimanhero1555@gmail.com>
2025-06-04 20:29:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json()); 
-
-const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
